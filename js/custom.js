d9bcb3eb72b5a922ff14b97689d4c7328dee713d
Md Sohel Rana Kaocher <kaocheraub@gmail.com>
2021-06-17 08:04:24 +0600

    boom

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade:true,
+        speed:1500,
+        arrows:false,
+      });
+
+});
